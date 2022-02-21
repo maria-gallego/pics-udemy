216b944484c381dc2b64154d1ddfa6e40f375604
maria-gallego <mariapaulagallegoc@gmail.com>
2022-02-22 08:17:11 +1100

    first commit

---
+++ src/api/unsplash.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+axios.create({
+    baseURL: 'https://api.unsplash.com',
+    headers: {
+        Authorization: 'Client-ID EIiu72TWxFoEyL9isbvZL-1AoPQ2QHTSLlq6wIqVgUI'
+    }
+})
